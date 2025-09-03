8a0824e23ea2a9039f092eeab0248a61a947e693
Crab_Void <Ralfs.Egle28@proton.me>
2025-09-03 13:23:07 +0300

    all tasks done

--- tasks.php
+++ tasks.php
@@ -1,9 +1,21 @@
 <?php
-# while (true) {
-    #$input = readline("Ievade> ");
-   # if ($input === "exit") {
-  #      break;
- #} 
-#}
-
-
+$tasks = ["build a house", "create the universe", "bet on a horse"];
+ while (true) {
+    $input = readline("Ievade> ");
+    if ($input === "kys") {
+        break;
+ } 
+     switch ($input) {
+    case 0:
+        echo $tasks[0], "\n";
+        break;
+    case 1:
+        echo $tasks[1], "\n";
+        break;
+    case 2:
+        echo $tasks[2], "\n";
+        break;
+    }
+}
+#print_r($tasks);
+?>
